7c941d3ccb27b3765f3478cf9d91f8c76c96af55
Asith Mallika Kankanamalage <93966973+Asithmk@users.noreply.github.com>
2026-04-22 09:26:29 +0200

    Update Wardrobe.java

--- src/main/java/de/tum/cit/fop/Wardrobe.java
+++ src/main/java/de/tum/cit/fop/Wardrobe.java
@@ -1,1 +1,32 @@
+package de.tum.cit.fop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Wardrobe {
+    private List<String> stack;
+
+    public Wardrobe() {
+        this.stack = new ArrayList<>();
+    }
+
+    public List<String> getStack() {
+        return stack;
+    }
+
+    public void setStack(List<String> stack) {
+        this.stack = stack;
+    }
+
+    public void addItemToStack(String item) {
+        stack.add(item);
+    }
+
+    public void takeItemFromStack() {
+        if (!stack.isEmpty()) {
+            String item = stack.remove(stack.size() - 1);
+            System.out.println(item + " is hanging");
+        }
+    }
+}
 
