85c5e1e71d981647137fcc74c5e7c569806d9225
Asith Mallika Kankanamalage <93966973+Asithmk@users.noreply.github.com>
2026-04-22 09:26:01 +0200

    Update Customer.java

--- src/main/java/de/tum/cit/fop/Customer.java
+++ src/main/java/de/tum/cit/fop/Customer.java
@@ -1,1 +1,39 @@
+package de.tum.cit.fop;
+
+public class Customer {
+    private String name;
+    private String carriedItem;
+
+    public Customer(String name, String carriedItem) {
+        this.name = name;
+        this.carriedItem = carriedItem;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCarriedItem() {
+        return carriedItem;
+    }
+
+    public void setCarriedItem(String carriedItem) {
+        this.carriedItem = carriedItem;
+    }
+
+    public void successfullyEnteredHall(Wardrobe wardrobe) {
+        System.out.println("YAY! " + name + " entered the hall");
+        wardrobe.addItemToStack(this.carriedItem);
+        carriedItem = null;
+    }
+
+    public void failedToEnterHall() {
+        System.out.println(name + ", don't cry bro :(");
+    }
+
+}
 
